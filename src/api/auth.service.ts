32f7da1921227a756268fe5b2780ce605d10ea0a
IslamAsankojoev <isla.asankojoev@mail.ru>
2023-06-15 15:02:15 +0600

    init auth

---
+++ src/api/auth.service.ts
@@ -0,0 +1,24 @@
+import axiosInstance from './axios.config';
+
+export const AuthService = {
+  login: async (username: string, password: string) => {
+    const response = await axiosInstance.post('/api/token/', { username, password });
+    const data = await response.data;
+    return data;
+  },
+  register: async (username: string, email: string, password: string, is_author: boolean) => {
+    const response = await axiosInstance.post('/users/', {
+      username,
+      email,
+      password,
+      is_author,
+    });
+    const data = await response.data;
+    return data;
+  },
+};
+
+export type ILogin = {
+  email: string;
+  password: string;
+};
